5288d73feb579cb561311483cffc5a9a890daeb9
Neelesh <pant20ec064@rmkcet.ac.in>
2024-11-27 16:38:36 +0530

    3nd commit

---
+++ Practice_Problems/Non-Repeating_Elements.py
@@ -0,0 +1,18 @@
+arr=[]
+n=int(input("Enter array size "))
+for i in range(0,n):
+    a=int(input())
+    arr.append(a)
+arr.sort()
+
+arr2=[]
+i=0
+while i < n-1:
+    if(arr[i]!=arr[i+1]):
+        b=arr[i]
+        arr2.append(b)
+        #arr.remove(i+1)
+    i=i+1
+d=len(arr2)
+for j in range(0,d):
+    print(arr2[j],end=" ")
